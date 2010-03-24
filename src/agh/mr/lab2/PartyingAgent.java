8ca7aa9a63acfd971b33653ee692b1adbdd3ccbe
orzechowski.michal <orzechowski.michal@344bffec-20bf-576c-abad-675d38c8f02f>
2010-03-24 21:08:39 +0000

    szkielet

---
+++ src/agh/mr/lab2/PartyingAgent.java
@@ -0,0 +1,11 @@
+package agh.mr.lab2;
+
+/**
+ * created at: Mar 24, 2010, 10:53:56 AM
+ *
+ * @author: Michal Orzechowski
+ */
+public interface PartyingAgent {
+
+      public boolean isGoingToParty() ;
+}
